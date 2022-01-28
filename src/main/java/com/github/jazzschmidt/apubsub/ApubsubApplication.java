26150380ffbe732c6e4c2a75ff0f9ef93e4898ff
Carsten Schmidt <jazzschmidt@icloud.com>
2022-01-28 17:06:35 +0100

    Adds generated Spring Boot template from Spring Initializr

---
+++ src/main/java/com/github/jazzschmidt/apubsub/ApubsubApplication.java
@@ -0,0 +1,13 @@
+package com.github.jazzschmidt.apubsub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApubsubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApubsubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/jazzschmidt/apubsub/ApubsubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jazzschmidt.apubsub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApubsubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
